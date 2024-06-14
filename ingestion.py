a3ec937091ccec67edfc5c20df77ee61e510ce66
Ankur Kushwaha <ankur.kushwaha@hcode.tech>
2024-06-14 13:11:30 +0530

    Read langchain documentation and push chunks into pinecone vector db

--- ingestion.py
+++ ingestion.py
@@ -1,35 +1,41 @@
-from dotenv import load_dotenv
+import os
 
-load_dotenv()
-
+OPENAI_API_KEY = ""
+os.environ["openai_api_key"] = OPENAI_API_KEY
 
 from langchain_community.document_loaders import ReadTheDocsLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
-
+from pinecone import Pinecone
 
 INDEX_NAME = "langchain-doc-index"
+PINECONE_API_KEY = ""
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
 
 
 def ingest_docs():
-    loader = ReadTheDocsLoader(
-        "langchain-docs/api.python.langchain.com/en/latest/chains"
-    )
+    loader = ReadTheDocsLoader("langchain-docs/langchain.readthedocs.io/en/latest")
 
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents)} documents")
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
+    )
     documents = text_splitter.split_documents(raw_documents)
+    print(f"Splitted into {len(documents)} chunks")
+
     for doc in documents:
         new_url = doc.metadata["source"]
         new_url = new_url.replace("langchain-docs", "https:/")
         doc.metadata.update({"source": new_url})
 
     print(f"Going to add {len(documents)} to Pinecone")
+
+    embeddings = OpenAIEmbeddings()
     PineconeVectorStore.from_documents(documents, embeddings, index_name=INDEX_NAME)
     print("****Loading to vectorstore done ***")
 
