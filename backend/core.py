2d5b1cbaeefdd17cad3cbdb51d040c9c13507404
Ankur Kushwaha <ankur.kushwaha@hcode.tech>
2024-06-19 19:09:38 +0530

    Added memory context to chatbot

--- backend/core.py
+++ backend/core.py
@@ -1,12 +1,12 @@
 import os
-from typing import Any
+from typing import Any, Dict, List
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from pinecone import Pinecone
 from langchain_community.vectorstores import Pinecone as PineconeLangChain
 
@@ -14,19 +14,25 @@ pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 from consts import INDEX_NAME
 
 
-def run_llm(query: str) -> Any:
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
     embeddings = OpenAIEmbeddings()
     docsearch = PineconeLangChain.from_existing_index(
         index_name=INDEX_NAME, embedding=embeddings
     )
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
+    # qa = RetrievalQA.from_chain_type(
+    #     llm=chat,
+    #     chain_type="stuff",
+    #     retriever=docsearch.as_retriever(),
+    #     return_source_documents=True,
+    # )
+
+    qa = ConversationalRetrievalChain.from_llm(
         llm=chat,
-        chain_type="stuff",
         retriever=docsearch.as_retriever(),
         return_source_documents=True,
     )
-    return qa({"query": query})
+    return qa({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
